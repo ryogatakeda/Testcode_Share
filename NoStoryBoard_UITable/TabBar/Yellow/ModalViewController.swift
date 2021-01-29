5136590dcc2b387ab5f8c0c15f43ef22a1ff7e0c
ryogatakeda <takedar59@gmail.com>
2021-01-29 21:24:16 +0900

    ModalViewController.swift追加

---
+++ NoStoryBoard_UITable/TabBar/Yellow/ModalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ModalViewController.swift
+//  NoStoryBoard_UITable
+//
+//  Created by 武田遼河 on 2021/01/24.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+    }
+
+
+}
